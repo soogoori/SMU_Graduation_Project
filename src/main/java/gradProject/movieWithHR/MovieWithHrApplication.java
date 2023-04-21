b1a0741ba8aa616faef0b2d93d39c894e5389061
soogoori <soobin0209@naver.com>
2023-04-22 01:10:11 +0900

    :sparkles: 기초 환경 세팅 완료

---
+++ src/main/java/gradProject/movieWithHR/MovieWithHrApplication.java
@@ -0,0 +1,13 @@
+package gradProject.movieWithHR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieWithHrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieWithHrApplication.class, args);
+	}
+
+}
